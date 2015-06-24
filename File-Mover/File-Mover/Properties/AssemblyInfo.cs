986da9dec20060588a0a5e4688f9a594479b8e53
James MacBeth <maccyjam@gmail.com>
2015-06-24 16:15:30 +0100

    Behind the scenes stuff. NET Framework version reduced to 3.5.

--- File-Mover/File-Mover/Properties/AssemblyInfo.cs
+++ File-Mover/File-Mover/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("File-Mover")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Moves files back and forth!")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Maccyjam")]
 [assembly: AssemblyProduct("File-Mover")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Maccyjam  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
